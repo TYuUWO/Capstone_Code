6a4a9e57157ebd2f355d8ea27a0a1800b64d2aa2
TYuUWO <tyu263@uwo.ca>
2023-03-06 12:01:16 -0500

    Added MultiRL

---
+++ Project/MultiRL.m
@@ -0,0 +1,3 @@
+function [] = MultiRL()
+
+end
--- Project/RL_Only.m
+++ Project/RL_Only.m
@@ -7,15 +7,22 @@ function [Rvals,Lvals,valsMap] = RL_Only(ports,TFs)
     % ex. Y13: node 1 to node 3
     valsMap = strings(1,p2p);
     for i = 1:p2p
-        valsMap(i) = "n"+string(i);
+        valsMap(i) = "n"+string(ceil(i/max(size(TFs))))+string(mod(i,max(size(TFs))));
     end
-    % 1 Rval, 1 Lval per port to port
+    % 1 Rval, 1 Lval per port to port per matrix
     Rvals = zeros(1,p2p);
     Lvals = zeros(1,p2p);
     TF2 = zeros(size(TFs));
     % loop over TFs;
     % TFs matrix should be symmetric across diagonal
     for i = 1:p2p
+        if (i==1) 
+            % initial case (1,n)
+            TF2(1,n) = TFs(1,n);
+            % (1/L)/(s+R/L)
+            % c = 1/L, p = -R/L
+            
+        end
         % compare TF2 to TFs
         % set Rval and Lval
         % add term to TF2
