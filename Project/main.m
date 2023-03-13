aa907905c8baf113ee8b1451eea9963e859af28a
TYuUWO <tyu263@uwo.ca>
2023-03-13 19:16:49 -0400

    Initial attempt at implementing state space to tf
    issue: transfer function form appears to not be simplified or outright wrong

--- Project/main.m
+++ Project/main.m
@@ -1,4 +1,23 @@
 %
 % main edit 3
 
+% extract A B C D E from SER
+A = SER.A;
+B = SER.B;
+C = SER.C;
+D = SER.D;
+E = SER.E;
+
 % use Y = [C((sI-A)^-1)B+D+sE]
+% can perform operations on tf struct
+% maybe create tf struct in place of sI and sE?
+s = tf([1 0],1);
+
+% A should be square matrix
+Isize = size(A);
+% identity matrix the size of A
+I = eye(max(Isize));
+%implement equation
+Y = (C*(inv((s*I)-A)))* B + D + (s*E);
+
+
