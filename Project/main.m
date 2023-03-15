21ef34b7e408146e5a8122fb7dfce9af1fbab289
TYuUWO <tyu263@uwo.ca>
2023-03-15 10:36:16 -0400

    commented out old code

--- Project/main.m
+++ Project/main.m
@@ -8,16 +8,23 @@ C = SER.C;
 D = SER.D;
 E = SER.E;
 
+% A are poles - refer to notes
+% C are residues
+% D constant
+
+
+
+% use for testing (validation) ONLY
 % use Y = [C((sI-A)^-1)B+D+sE]
 % can perform operations on tf struct
 % maybe create tf struct in place of sI and sE?
-s = tf([1 0],1);
+%s = tf([1 0],1);
 
 % A should be square matrix
-Isize = size(A);
+%Isize = size(A);
 % identity matrix the size of A
-I = eye(max(Isize));
+%I = eye(max(Isize));
 %implement equation
-Y = (C*(inv((s*I)-A)))* B + D + (s*E);
+%Y = (C*(inv((s*I)-A)))* B + D + (s*E);
 
 
